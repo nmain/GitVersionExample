11c71af1ff6f50ad88ca27605f42d6722e741838
Nicholai Main <nmain@measinc.com>
2016-12-19 09:29:04 -0500

    Example application

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var version = typeof(Program)
+                .Assembly
+                .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                .InformationalVersion;
+
+            Console.WriteLine("Hello, world!");
+            Console.WriteLine($"I am version {version}");
+        }
+    }
+}
